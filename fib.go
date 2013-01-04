3d40bc802041f887ffcec32263bd2f8f3843a24a
mythril <lucas.m.green@gmail.com>
2013-01-04 03:43:11 -0800

    Added: basic fibonaci function with memoization

---
+++ fib.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fibber() func(int) uint64 {
+	memo := make(map[int]uint64)
+	var realfib func(int) uint64
+	realfib = func(a int) uint64 {
+		if a < 0 {
+			return 0
+		}
+		if a == 0 {
+			return 1
+		}
+		if a == 1 {
+			return 1
+		}
+		r, found := memo[a]
+		if found == false {
+			memo[a] = realfib(a-1) + realfib(a-2)
+			r = memo[a]
+		} else {
+			fmt.Println("Found:", r, "at index:", a)
+		}
+		return r
+	}
+	return realfib
+}
+
+func main() {
+	fib := fibber()
+	for i := 0; i < 100; i += 1 {
+		fmt.Println("fib("+strconv.Itoa(i)+") == ", fib(i))
+	}
+}
